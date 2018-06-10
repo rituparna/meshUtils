04ee4b995a1af7c350192a4e02083298bc6eefde
rituparna <royrit@gmail.com>
2018-06-10 15:34:22 -0700

    Update dagNameFromSelection.py
    adding check

--- dagNameFromSelection.py
+++ dagNameFromSelection.py
@@ -1,6 +1,9 @@
 import maya.api.OpenMaya as om2
-
+##works on selection in maya scene
 selectList=om2.MGlobal.getActiveSelectionList()
 dagPath=om2.MDagPath()
-dagPath=selectList.getDagPath(0)
-print dagPath.fullPathName()
+if (selectList.length()<1):
+    raise Exception('select atleast one object')
+else:
+    dagPath=selectList.getDagPath(0)
+    print dagPath.fullPathName()
